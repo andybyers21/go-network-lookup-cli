dfaccd995656aa750a11b2c15b8bfff18b7292e1
andybyers21 <andy.byers21@gmail.com>
2021-03-25 13:58:23 +0000

    add vendor module

---
+++ cmd/cli/cli.go
@@ -0,0 +1,10 @@
+// cmd/my-cli/cli.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Go Network Lookup CLI v0.01")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module network-lookup-cli
 
 go 1.15
+
+require github.com/urfave/cli v1.22.5 // indirect
