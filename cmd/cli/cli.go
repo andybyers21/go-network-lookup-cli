b4172e277e4fe58ca6edd7e2479affb09b42a544
andybyers21 <andy.byers21@gmail.com>
2021-03-25 14:18:26 +0000

    create a new CLI application

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -3,8 +3,17 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
 )
 
 func main() {
 	fmt.Println("Go Network Lookup CLI v0.01")
+
+	err := cli.NewApp().Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
